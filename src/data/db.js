dc8551b238fda258c75996cbc8885c6c17e62397
Mielcarek, J.J. (Jacob) <jcmielcarek@gmail.com>
2024-12-05 21:39:28 +0100

    Update db.js

--- src/data/db.js
+++ src/data/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: process.env.MYSQL_HOST,
     user: 'root',
     password: '',
     database: 'tsp',
